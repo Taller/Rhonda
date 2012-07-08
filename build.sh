addbd911aaabdfa9947ba357e7d9183965e990d8
Taller <clever@bk.ru>
2012-07-09 10:42:18 +1100

    build script

--- build.sh
+++ build.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 echo "Compiling implementation"
-gcc -fPIC -g -c -Wall src/linear_sequence.c -Iheaders
+gcc -fPIC -c -o build/liblsq.o src/linear_sequence.c -Iheaders
+ld -shared -soname liblsq.so.1 -o ./build/liblsq.so.1.0 -lc ./build/liblsq.o
+#gcc -fPIC -g -c -Wall src/linear_sequence.c -Iheaders
 
-echo "Linking to shared library"
-gcc -shared -Wl,-soname,lib_lsq.so.1 \
-    -o build/lib_lsq.so.1.0.1 linear_sequence.o  -lc
+#echo "Linking to shared library"
+#gcc -shared -Wl,-soname,lib_lsq.so.1 \
+#    -o build/lib_lsq.so.1.0.1 linear_sequence.o  -lc
     
 echo "Preparing local library cache"
 ldconfig -v -n ./build
 
 echo "Making symbolic link"
-ln -sf lib_lsq.so.1 build/lib_lsq.so 
+ln -sf liblsq.so.1 ./build/liblsq.so 
 
 echo "Preparing enviroment"
-export LD_LIBRARY_PATH=.:build:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=.build/:$LD_LIBRARY_PATH
 
-gcc -o run_tests tests/lsq_tests.c -Iheaders -L./build  -llib_lsq 
+gcc -o run_tests tests/lsq_tests.c -Iheaders -L./build -lliblsq
