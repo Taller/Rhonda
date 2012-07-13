83b32c35eeacc99b81d3bab54d3229832b467da7
Taller <clever@bk.ru>
2012-07-13 11:23:12 +1100

    Renamed headers to include

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 all:
-	@cc -fPIC -c -o build/liblsq.o src/linear_sequence.c -I headers
+	@cc -fPIC -c -o build/liblsq.o src/linear_sequence.c -I include
 	@ld -shared -soname liblsq.so.1 -o ./build/liblsq.so.1.0 -lc ./build/liblsq.o
 	@ldconfig -n ./build
 	@ln -sf liblsq.so.1 ./build/liblsq.so 
-	@gcc -c -o build/lsq_tests.o tests/lsq_tests.c -I headers
-	@gcc -o run_tests -I headers -L./build -llsq build/lsq_tests.o
+	@gcc -c -o build/lsq_tests.o tests/lsq_tests.c -I include
+	@gcc -o run_tests -I include -L./build -llsq build/lsq_tests.o
 	@LD_LIBRARY_PATH=.:./build:D_LIBRARY_PATH ./run_tests
--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 rm build/*.*
 echo "Compiling implementation"
-gcc -fPIC -c -o build/liblsq.o src/linear_sequence.c -Iheaders
+gcc -fPIC -c -o build/liblsq.o src/linear_sequence.c -Iinclude
 ld -shared -soname liblsq.so.1 -o ./build/liblsq.so.1.0 -lc ./build/liblsq.o
 
 echo "Preparing local library cache"
@@ -15,9 +15,9 @@ echo "Preparing enviroment"
 export LD_LIBRARY_PATH=.:./build:$LD_LIBRARY_PATH
 
 echo "Compiling tests"
-gcc -c -o build/lsq_tests.o tests/lsq_tests.c -I headers 
+gcc -c -o build/lsq_tests.o tests/lsq_tests.c -I include 
 
 echo "Linking tests"
-gcc  -o run_tests -I headers -L./build -llsq build/lsq_tests.o
+gcc  -o run_tests -I include -L./build -llsq build/lsq_tests.o
 
 ./run_tests
